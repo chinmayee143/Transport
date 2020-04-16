67e7f60aeea14df694a8aa421c71abf22ecde471
ganeshShetty98 <gs00619@gmail.com>
2020-04-16 16:47:45 +0530

    Update ViewEvening.dart

--- lib/ViewEvening.dart
+++ lib/ViewEvening.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 
 import 'package:url_launcher/url_launcher.dart';
 
-
 class ViewEvening extends StatelessWidget {
   const ViewEvening({Key key}) : super(key: key);
 
@@ -19,40 +18,35 @@ class ViewEvening extends StatelessWidget {
       ),
       body: Container(
         padding: EdgeInsets.all(20.0),
-
         child: ListView(
           children: <Widget>[
             Card(
-                                  elevation: 10,
-                                  margin: EdgeInsets.all(8.0),
-                                  child: InkWell(
-                                    onTap: () {
-                                       launch(
-                            'https://drive.google.com/file/d/1tRtH-cPvVpKNej5zAQEz6A-O-L2KCqyg/view');
-                            
-
-
-                                    },
-                                    splashColor: Colors.lightBlueAccent,
-                                    child: Row(
-                                      mainAxisSize: MainAxisSize.min,
-                                      children: <Widget>[
-                                        
-                                        Flexible(
-                                          child: Text(
-                                            "View all routes",
-                                            style: new TextStyle(fontSize: 25.0),
-                                            textAlign: TextAlign.center,
-                                          ),
-                                          fit: FlexFit.tight,
-                                        )
-                                      ],
-                                    ),
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
-                        );
-                      }
-                    }
+              elevation: 10,
+              margin: EdgeInsets.all(8.0),
+              child: InkWell(
+                onTap: () {
+                  launch(
+                      'https://drive.google.com/file/d/1tRtH-cPvVpKNej5zAQEz6A-O-L2KCqyg/view');
+                },
+                splashColor: Colors.lightBlueAccent,
+                child: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    Flexible(
+                      child: Text(
+                        "View all routes",
+                        style: new TextStyle(fontSize: 25.0),
+                        textAlign: TextAlign.center,
+                      ),
+                      fit: FlexFit.tight,
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
