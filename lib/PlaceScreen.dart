8a0085eea3bd61a382c5fa2d13f5299f00361318
ganeshShetty98 <gs00619@gmail.com>
2020-04-16 16:47:55 +0530

    Update PlaceScreen.dart

--- lib/PlaceScreen.dart
+++ lib/PlaceScreen.dart
@@ -3,7 +3,6 @@ import 'package:transport/MDSearch.dart';
 import 'package:transport/MHSearch.dart';
 import 'package:transport/View.dart';
 
-
 class PlaceScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -12,11 +11,9 @@ class PlaceScreen extends StatelessWidget {
         title: Text('Select Place'),
         backgroundColor: Colors.blue[300],
         elevation: 10.0,
-
       ),
       body: Container(
         padding: EdgeInsets.all(20.0),
-
         child: ListView(
           children: <Widget>[
             Card(
@@ -24,8 +21,10 @@ class PlaceScreen extends StatelessWidget {
               margin: EdgeInsets.all(8.0),
               child: InkWell(
                 onTap: () {
-                  Navigator.push(context,
-                    MaterialPageRoute(builder: (context) =>MDSearch()),);
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => MDSearch()),
+                  );
                 },
                 splashColor: Colors.lightBlueAccent,
                 child: Row(
@@ -47,67 +46,69 @@ class PlaceScreen extends StatelessWidget {
                 ),
               ),
             ),
-             Card(
-                                  elevation: 10,
-                                  margin: EdgeInsets.all(8.0),
-                                  child: InkWell(
-                                    onTap: () {
-                                      Navigator.push(context,
-                                        MaterialPageRoute(builder: (context) =>MHSearch()),);
-                                    },
-                                    splashColor: Colors.lightBlueAccent,
-                                    child: Row(
-                                      mainAxisSize: MainAxisSize.min,
-                                      children: <Widget>[
-                                        Icon(
-                                          Icons.place,
-                                          size: 50.0,
-                                        ),
-                                        Flexible(
-                                          child: Text(
-                                            "Hubli",
-                                            style: new TextStyle(fontSize: 25.0),
-                                            textAlign: TextAlign.center,
-                                          ),
-                                          fit: FlexFit.tight,
-                                        )
-                                      ],
-                                    ),
-                                  ),
-                                ),
-                             Card(
-                                  elevation: 10,
-                                  margin: EdgeInsets.all(8.0),
-                                  child: InkWell(
-                                    onTap: () {
-                                      Navigator.push(context,
-                                        MaterialPageRoute(builder: (context) =>View()),);
-                                    },
-                                    splashColor: Colors.lightBlueAccent,
-                                    child: Row(
-                                      mainAxisSize: MainAxisSize.min,
-                                      children: <Widget>[
-                                        Icon(
-                                          Icons.place,
-                                          size: 50.0,
-                                        ),
-                                        Flexible(
-                                          child: Text(
-                                            "View all routes",
-                                            style: new TextStyle(fontSize: 25.0),
-                                            textAlign: TextAlign.center,
-                                          ),
-                                          fit: FlexFit.tight,
-                                        )
-                                      ],
-                                    ),
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
-                        );
-                      }
-                    }
-                    
-                    
+            Card(
+              elevation: 10,
+              margin: EdgeInsets.all(8.0),
+              child: InkWell(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => MHSearch()),
+                  );
+                },
+                splashColor: Colors.lightBlueAccent,
+                child: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    Icon(
+                      Icons.place,
+                      size: 50.0,
+                    ),
+                    Flexible(
+                      child: Text(
+                        "Hubli",
+                        style: new TextStyle(fontSize: 25.0),
+                        textAlign: TextAlign.center,
+                      ),
+                      fit: FlexFit.tight,
+                    )
+                  ],
+                ),
+              ),
+            ),
+            Card(
+              elevation: 10,
+              margin: EdgeInsets.all(8.0),
+              child: InkWell(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => View()),
+                  );
+                },
+                splashColor: Colors.lightBlueAccent,
+                child: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    Icon(
+                      Icons.place,
+                      size: 50.0,
+                    ),
+                    Flexible(
+                      child: Text(
+                        "View all routes",
+                        style: new TextStyle(fontSize: 25.0),
+                        textAlign: TextAlign.center,
+                      ),
+                      fit: FlexFit.tight,
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
